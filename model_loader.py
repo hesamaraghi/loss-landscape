aab48fcc163cdd5520dbecdea849a86de29f1071
hesamaraghi <hesam.araghi@gmail.com>
2024-01-29 17:22:46 +0100

    add the parent project to python path

--- model_loader.py
+++ model_loader.py
@@ -1,12 +1,34 @@
 import os
-import torch
 import cifar10.model_loader
+import sys
+from omegaconf import OmegaConf
+import torch
+ 
+# getting the name of the directory
+# where the this file is present.
+current = os.path.dirname(os.path.realpath(__file__))
+ 
+# Getting the parent directory name
+# where the current directory is present.
+parent = os.path.dirname(current)
+ 
+# adding the parent directory to 
+# the sys.path.
+sys.path.append(parent)
+
+
+import model_factory
+import os
+
 
 def load(dataset, model_name, model_file, data_parallel=False):
     if dataset == 'cifar10':
         net = cifar10.model_loader.load(model_name, model_file, data_parallel)
     else:
-        net = torch.load(model_file)
+        cfg_file = os.path.join(os.path.dirname(model_file), "cfg.yaml")
+        cfg = OmegaConf.load(cfg_file)
+        net = model_factory.factory(cfg)
+        net.load_state_dict(torch.load(model_file))
     return net
 
 
